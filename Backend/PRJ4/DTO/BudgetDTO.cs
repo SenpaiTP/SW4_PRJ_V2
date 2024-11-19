59de6859fde8ca6aca4b4507d6e7cd0f54dc6d90
Thea Rønneholt <202210051@post.au.dk>
2024-11-19 21:41:01 +0100

    Create budgetDTO

---
+++ Backend/PRJ4/DTO/BudgetDTO.cs
@@ -0,0 +1,8 @@
+namespace PRJ4.DTOs;
+public class BudgetDTO
+{
+    public int? BrugerId { get; set; }
+    public int SavingsGoal { get; set; }
+    public int MonthlySavingsAmount { get; set; }
+    public int SavingsPeriodInMonths { get; set; }
+}
