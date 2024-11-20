e6921b591db47a4520e769df30d4dcb99ee70bd8
SenpaiTP <thomascool01@gmail.com>
2024-11-20 09:44:00 +0100

    Added missing frontend

---
+++ Frontend/KontoKaren/src/Pages/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function Homepage() {
+  return (
+    <Container>
+      <Typography variant="h1" component="h2" gutterBottom>
+        Welcome to the Homepage
+      </Typography>
+    </Container>
+  );
+}
+
+export default Homepage;
+
+// test
