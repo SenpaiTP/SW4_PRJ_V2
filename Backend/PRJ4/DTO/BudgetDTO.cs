d7fbc95fb2f59b5147c0764597408466551f5a4d
Thea Rønneholt <202210051@post.au.dk>
2024-11-26 16:43:11 +0100

    Update BudgetDTO.cs
    Made new properties in BudgetDTOs

--- Backend/PRJ4/DTO/BudgetDTO.cs
+++ Backend/PRJ4/DTO/BudgetDTO.cs
@@ -1,18 +1,25 @@
 namespace PRJ4.DTOs;
 
 
-public class BudgetDTO
+public class BudgetCreateDTO
 {
+    public string BudgetName { get; set; }
+
     public int SavingsGoal { get; set; }
-    public DateOnly BudgetStart { get; set; }
+
     public DateOnly BudgetSlut { get; set; }
 }
 
-public class BudgetGetDTO
+public class BudgetResponseDTO
 {
     public int BudgetId { get; set; }
+    public int BrugerId { get; set; }
+    public string BudgetName { get; set; }
     public int SavingsGoal { get; set; }
-    public DateOnly BudgetStart { get; set; }
     public DateOnly BudgetSlut { get; set; }
     public int MonthlySavingsAmount { get; set;}
+    public decimal MoneySaved { get; set; } 
+    //public decimal PercentOfGoal { get; set; }
+
+
 }
